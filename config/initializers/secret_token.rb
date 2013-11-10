a388353538ed8ebfee1790c30282b59b78db6a6b
hui <hui@networking.io>
2013-11-10 17:06:06 +0800

    init code.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JrubyRailsDemo::Application.config.secret_key_base = 'b74f6e81327d34ddb4fb52babd7809a82ab776d1a0b017711fee967b246c250b076287017944cfc8eab32b4e30cb8c8ad3b04e639203cbf997c52407632e421b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JrubyRailsDemo::Application.config.session_store :cookie_store, key: '_jruby-rails-demo_session'
